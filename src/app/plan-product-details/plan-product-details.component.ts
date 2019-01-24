1a8e9032420fce5aec89f081cf429ff13af5f48a
Sandeep SK <sandeep.sk@zoftsolutions.com>
2019-01-24 13:03:31 +0530

    Add services

--- src/app/plan-product-details/plan-product-details.component.ts
+++ src/app/plan-product-details/plan-product-details.component.ts
@@ -11,16 +11,18 @@ import * as _ from "lodash"
 })
 export class PlanProductDetailsComponent implements OnInit {
 
-  planProducts$ : Object;
-  planDetails$ : Object;
+  planProducts$: Object;
+  planDetails$: Object;
   grandTotal: number = 0;
   finalRes: Object;
-  total: number =0;
-  planarray:any = [];
+  total: number = 0;
+  planarray: any = [];
+
+
   //t: number=0;
 
-  constructor(private data:DataService, private route: ActivatedRoute ) { }
-  
+  constructor(private data: DataService, private route: ActivatedRoute) { }
+
   ngOnInit() {
     var subId;
     this.route.params.subscribe(params => {
@@ -28,83 +30,143 @@ export class PlanProductDetailsComponent implements OnInit {
       subId = params.id;
     })
     this.data.getPlanProducts(subId).subscribe(
-      data=>{this.planProducts$=data},
+      data => { this.planProducts$ = data },
       err => {
-          console.log(err)
-      }, () =>{ 
-        
-        this.test()}
+        console.log(err)
+      }, () => {
+
+        if (this.planProducts$[0].orderToCashCycles.length == 1) {
+
+          this.getTotalAmount(this.planProducts$[0].orderToCashCycles[0].planFrequencyId, "init")
+        } else {
+          this.getTotalAmount(this.planProducts$[0].orderToCashCycles[1].planFrequencyId, "init")
+        }
+
+
+
+      }
 
     );
 
     this.data.getPlanDetails(subId).subscribe(
 
-      data=>{this.planDetails$=data},
+      data => { this.planDetails$ = data },
       err => {
-          console.log(err)
-      }, () =>{ 
-        
+        console.log(err)
+      }, () => {
+
       }
-     
+
     );
 
-  
 
 
 
-   
-  }
- 
-  test(){
- 
- 
- 
-  let sum = 0;
-   
-  for(let products of _.values(this.planProducts$)){
 
-// if(products.productType == 'RecurringService' && products.isIncludedByDefault == false){
 
-// }else{
-// sum+=products.orderToCashCycles[0].pricingModel.quantityRanges[0].prices[0].amount;
-// }
-sum+=products.orderToCashCycles[0].pricingModel.quantityRanges[0].prices[0].amount;
-  }
-  this.grandTotal = sum;
-  return this.grandTotal;
-}
-   
-    
-  
-
-  yourfunc(addonvalue,event){
-   
-  if(event.target.checked){
-    this.grandTotal = this.grandTotal+parseInt(addonvalue);
-  }else{
-    this.grandTotal = this.grandTotal-parseInt(addonvalue);
-  }
-    
- //this.test('s',this.grandTotal);
-    
   }
 
-  subscribe(s){
+  getTotalAmount(plnID, from) {
 
-    
-    this.data.createSub(s,"4848884");
+    let sum = 0;
+    let count = 0;
+    for (let j = 0; j < JSON.stringify(this.planProducts$).length; j++) {
+
+
+      if (from == "change") {
+        for (let i = 0; i < this.planProducts$[j].orderToCashCycles.length; i++) {
+
+          //alert(this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount);
+          // if(products.productType == 'RecurringService' && products.isIncludedByDefault == false){
+
+          // }else{
+          // sum+=products.orderToCashCycles[0].pricingModel.quantityRanges[0].prices[0].amount;
+          // }
+          //alert(this.planProducts$[j].orderToCashCycles[i].planFrequencyId)
+
+
+          if (this.planProducts$[j].orderToCashCycles[i].planFrequencyId == plnID) {
+
+            sum = sum + this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount;
+            this.grandTotal = sum;
+
+            document.getElementById("test4" + j).innerHTML = "$" + this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount;
+
+
+          }
+
+
+
+
+        }
+      } else {
+        for (let i = 0; i < this.planProducts$[j].orderToCashCycles.length; i++) {
+
+          //alert(this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount);
+          // if(products.productType == 'RecurringService' && products.isIncludedByDefault == false){
+
+          // }else{
+          // sum+=products.orderToCashCycles[0].pricingModel.quantityRanges[0].prices[0].amount;
+          // }
+          //alert(this.planProducts$[j].orderToCashCycles[i].planFrequencyId)
+
+
+          if (this.planProducts$[j].orderToCashCycles[i].planFrequencyId == plnID) {
+
+            sum = sum + this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount;
+            this.grandTotal = sum;
+
+            //document.getElementById("test4"+j).innerHTML = "$"+this.planProducts$[j].orderToCashCycles[i].pricingModel.quantityRanges[0].prices[0].amount;
+
+
+          }
+
+
+
+
+        }
+
+      }
+      count++;
+    }
+
+    return this.grandTotal;
   }
 
-  addtocart(planDetails){
-    this.planarray=localStorage.getItem("Plans");
-    if(this.planarray==null){
-      this.planarray=[];
-    }else{
 
-this.planarray.push(planDetails);
-localStorage.setItem("Plans",this.planarray);
-alert(this.planarray.length)
+
+
+  yourfunc(addonvalue, event) {
+
+    if (event.target.checked) {
+      this.grandTotal = this.grandTotal + parseInt(addonvalue);
+    } else {
+      this.grandTotal = this.grandTotal - parseInt(addonvalue);
+    }
+
+    //this.test('s',this.grandTotal);
+
   }
-}
-  producttype=true;
+
+  subscribe(s) {
+
+
+    this.data.createSub(s, "4848884");
+  }
+
+  addtocart(planDetails) {
+    this.planarray = localStorage.getItem("Plans");
+    if (this.planarray == null) {
+      this.planarray = [];
+    } else {
+
+      this.planarray.push(planDetails);
+      localStorage.setItem("Plans", this.planarray);
+      alert(this.planarray.length)
+    }
+  }
+  getAmount(plnID) {
+    this.getTotalAmount(plnID, "change")
+  }
+  producttype = true;
 }
