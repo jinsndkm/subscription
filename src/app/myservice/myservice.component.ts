bef6374b8bf498f4d4997f3668bec433b3b7f08a
unknown <Jins>
2019-02-20 17:06:29 +0530

    Check already subscribed services

--- src/app/myservice/myservice.component.ts
+++ src/app/myservice/myservice.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { from } from 'rxjs';
 import { HideMenusService } from '../hide-menus.service';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import * as $ from "jquery";
 import * as _ from "lodash"
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -17,10 +17,12 @@ enableProdMode();
 export class MyserviceComponent implements OnInit {
 
   servicesList$: Object;
+  finalServicesList$: any[] = [];
   mysubscriptions$: Object;
-
+  serviceList: any = [];
+  serviceStatus: boolean = false;
   private modals: any[] = [];
-plans:any = [];
+  plans: any = [];
   constructor(private data: DataService, public nav: HideMenusService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
@@ -30,36 +32,53 @@ plans:any = [];
     this.data.getAllFusebillServices().subscribe(
       //data => this.servicesList$ = data
       data => {
-      var json=JSON.parse(JSON.stringify(data));
-      this.servicesList$=json.data;
+        var serviceJson = JSON.parse(JSON.stringify(data));
+        this.servicesList$ = serviceJson.data;
+
+
+        this.data.getMySubscription().subscribe(
+
+          data => { //this.mysubscriptions$ = data ;
+            var json = JSON.parse(JSON.stringify(data));
+            this.mysubscriptions$ = json.data;
+            // Checking service is already subscribed or not
+            for (var i = 0; i < serviceJson.data.length; i++) {
+              var status = 0;
+              for (var j = 0; j < json.data.length; j++) {
+                if (serviceJson.data[i].id == json.data[j].items.data[0].plan.product) {
+                  status = 1;
+                }
+
+              }
+              if (status == 0) {
+                this.finalServicesList$.push(this.servicesList$[i]);
+              }
+            }
+            ////////////////////////////////////////////////
+          },
+          err => {
+            console.log(err)
+          }, () => {
+          }
+
+        );
+
+
       },
       err => {
         console.log(err)
       }, () => {
         this.spinner.hide();
       }
-      
-    );
-
-    this.data.getMySubscription().subscribe(
-
-      data => { //this.mysubscriptions$ = data ;
-        var json = JSON.parse(JSON.stringify(data));
-        this.mysubscriptions$=json.data;
-        
-      },
-      err => {
-        console.log(err)
-      }, () => {
-      }
 
     );
 
 
-    
 
 
-  userName: String;
 
-}
+
+    userName: String;
+
+  }
 }
